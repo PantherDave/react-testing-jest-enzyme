ffc016eee5f3640ef230d131131f379f774bcb4b
PantherDave <dacos062@fiu.edu>
2021-12-01 18:31:08 -0500

    GuessedWords component testing initialized

---
+++ jotto/src/GuessedWords.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkPT } from "../test/testUtils";
+import GuessedWords from "./GuessedWords";
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }],
+};
+
+/**
+ * Factory function to create a ShallowWrapper for the GuessedWords component.
+ * @function setup
+ * @param {object} props - Component porps specific to this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
+
+test("does not throw warning with expected props", () => {
+  checkPT(GuessedWords, defaultProps);
+});
