0615a16e7c4be46a0f4dd1c479208bb013eceb42
PantherDave <dacos062@fiu.edu>
2022-01-03 22:01:28 -0500

    Test useEffect for getSecretWord

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -4,12 +4,18 @@ import "./App.css";
 import Congrats from "./Congrats";
 import GuessedWords from "./GuessedWords";
 import Input from "./Input";
+import { getSecretWord } from "./actions";
+import { useEffect } from "react";
 
 function App() {
   // TODO: get props form shared state
   const success = false;
   const secretWord = "party";
   const guessedWords = [];
+
+  useEffect(() => {
+    getSecretWord();
+  }, []);
 
   return (
     <div data-test="component-app" className="container">
