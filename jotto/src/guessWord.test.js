746be6cd3946b1ccdc2f8d54bfc15d5568df3a7a
PantherDave <dacos062@fiu.edu>
2022-01-03 20:29:00 -0500

    Testing axios with moxios

---
+++ jotto/src/guessWord.test.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import App from "./App";
+import { findByTestAttr } from "../test/testUtils";
+import Input from "./Input";
+
+/**
+ * Create wrapper with specified initial conditions,
+ * then submit a guessed word of 'train'
+ * @function
+ *
+ * @param {object} state - initial conditions.
+ * @returns {Wrapper} - Enzyme wrapper of mounted App component
+ */
+const setup = (state = {}) => {
+  // TODO: apply state
+  const wrapper = mount(<App />);
+
+  // add value to input box
+  const inputBox = findByTestAttr(wrapper, "input-box");
+  inputBox.simulate("change", { target: "train" });
+
+  // simulate click on submit button
+  const submitButton = findByTestAttr(wrapper, "submit-button");
+  submitButton.simulate("click", { preventDefault() {} });
+
+  return wrapper;
+};
+
+// describe("invalid word guessed", () => {
+//   test.todo("guessedWords table does not get another row");
+// });
+
+describe.skip("no words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWords: [],
+    });
+  });
+  test("creates GuessedWords table with one row", () => {
+    const guessedWordRows = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordRows).toHaveLength(1);
+  });
+});
+
+describe.skip("some words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWords: [{ guessedWord: "agile", letterMatchCount: 1 }],
+    });
+  });
+  test("creates GuessedWords table with one row", () => {
+    const guessedWordRows = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordRows).toHaveLength(2);
+  });
+});
+
+describe.skip("guess secret word", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWords: [{ guessedWord: "agile", letterMatchCount: 1 }],
+    });
+
+    // add value to input box
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    const mockEvent = { target: { value: "party" } };
+
+    // simulate clock on submit button
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    submitButton.simulate("click", { preventDefault() {} });
+  });
+  test("adds row to guessedWords table", () => {
+    const guessedWordRows = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordRows).toHaveLength(3);
+  });
+  test("displays congrats component", () => {
+    const congrats = findByTestAttr(wrapper, "component-congrats");
+    expect(congrats.text().length).toBeGreaterThan(0);
+  });
+  test("does not display input component contents", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    expect(inputBox.exists()).toBe(false);
+
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    expect(submitButton.exists()).toBe(false);
+  });
+});
